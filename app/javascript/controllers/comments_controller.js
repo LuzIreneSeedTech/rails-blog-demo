a354aae82b9b15e0ab3d1eb48bd74f10376f5f8d
LuzIreneSeedTech <cortez.luzirene@seedtech.ph>
2024-03-12 14:32:58 +0800

    added cancel button for editing comment

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -10,13 +10,32 @@ export default class extends Controller {
 
     const formID = event.params["form"]
     const commentBodyID = event.params["body"]
+    const editButtonID = event.params["edit"]
 
     const form = document.getElementById(formID)
     form.classList.toggle("d-none")
     form.classList.toggle("mt-5")
 
-    const body = document.getElementById(commentBodyID)
-    body.classList.toggle("d-none")
+    const commentBody = document.getElementById(commentBodyID)
+    commentBody.classList.toggle("d-none")
+
+    const editButton = document.getElementById(editButtonID)
+    this.toggleEditButton(editButton)
+  }
+
+  toggleEditButton(editButton) {
+    if (editButton.innerText === "Edit") {
+      editButton.innerText = "Cancel"
+      this.toggleEditButtonClass(editButton)
+    } else {
+      editButton.innerText = "Edit"
+      this.toggleEditButtonClass(editButton)
+    }
+  }
+
+  toggleEditButtonClass(editButton) {
+    editButton.classList.toggle("btn-secondary")
+    editButton.classList.toggle("btn-warning")
   }
 }
 
